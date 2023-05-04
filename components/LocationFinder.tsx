3e715e42fa11543d0f0f753f380d9c3ddceb45a6
marcograhl <{ID}+{username}@users.noreply.github.com>
2023-05-04 18:22:53 +0200

    window object now is availible

--- components/LocationFinder.tsx
+++ components/LocationFinder.tsx
@@ -1,34 +1,34 @@
+'use client'
+import { Station } from "@/types/tankstellen-types";
 import dynamic from "next/dynamic";
 const Map = dynamic(() => import('@/components/Map'), {
   ssr: false,
 });
 
-// fetch data every 15min
-export const revalidate = 900;
 
-// Berlin
-const defaultCenter = { lat: 52.520008, lng: 13.404954 };
-const defaultZoom = 11;
-const defaultRadius = 15;
 
-// enviroment Variables
-const TANK_URL = process.env.TANK_URL
-const TANK_KEY = process.env.TANK_KEY
-
-// Fetch data from tankerkoenig
-const getLocations = async() => {
-  const response = await fetch(`${TANK_URL}list.php?lat=${defaultCenter.lat}&lng=${defaultCenter.lng}&rad=${defaultRadius}&sort=dist&type=all&apikey=${TANK_KEY}`)
-  return response.json();
+type Props = {
+  locations: Station[];
 }
 
-async function LocationFinder() {
-  const locations = await getLocations();
+const defaultZoom = 11;
+const defaultCenter = { lat: 52.520008, lng: 13.404954 };
+
+// Prüfen, ob Gerät Geolocation unterstützt
+const navigatorAvailable = Boolean(window?.navigator?.geolocation);
+
+
+
+function LocationFinder({locations}: Props) {
+
   return (
     <div>
+      {navigatorAvailable && 'hi'}    
       <Map zoom={defaultZoom} center={defaultCenter} locations={locations.stations} />
     </div>
   )
 }
+
 
 
 
